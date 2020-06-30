94c799b1538915cb202dff9bcfa8e67a2175c9ba
Adam <smolonek@gmail.com>
2020-06-30 20:50:12 +0200

    Updates

---
+++ precache-manifest.7b6b9838d3901622548e984a7f1c2e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f489bd8decc051d3f5c9661874744a35",
+    "url": "/react-routing-test/index.html"
+  },
+  {
+    "revision": "d4aba214781e37df2a90",
+    "url": "/react-routing-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7dee4e393ce47e5722f0",
+    "url": "/react-routing-test/static/js/2.82355200.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-routing-test/static/js/2.82355200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4aba214781e37df2a90",
+    "url": "/react-routing-test/static/js/main.412c3d14.chunk.js"
+  },
+  {
+    "revision": "e3907247e92fe8136081",
+    "url": "/react-routing-test/static/js/runtime-main.8d0c146b.js"
+  }
+]);
